04b48b15f51481347ded1fe7f52a12f4db6a3e66
Rocky Dunlap <dunlap@ucar.edu>
2022-08-05 04:11:52 -0700

    action=collect dir=intel_19.1.2_mpiuni_g_feature_large-messages branch=feature_large-messages hash=v8.3.0b17-27-g5b5163f phase=build

---
+++ feature_large-messages/intel/19.1.2/g/mpiuni/None/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=e3sm
+#SBATCH -o /global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/build.bat_%j.o
+#SBATCH -e /global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH -C haswell
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load intel/19.1.2.254 
+module load cray-netcdf/4.6.3.2
+
+set -x
+export var1
+export var2
+export var3
+export ESMF_DIR=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/module-build.log
+cd /global/cscratch1/sd/rsdunlap/esmf-testing/intel_19.1.2_mpiuni_g_feature_large-messages/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 32 2>&1| tee ../build.log
