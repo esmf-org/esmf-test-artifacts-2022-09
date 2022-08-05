c61da55a13a5bc738983f6f65de6e4526c787f5c
Mark A Potts <Mark.Potts@noaa.gov>
2022-08-05 01:23:25 +0000

    action=collect dir=gfortran_10.3.0_mpi_O_feature_large-messages branch=feature_large-messages hash=v8.3.0b17-27-g5b5163f phase=test

---
+++ feature_large-messages/gfortran/10.3.0/O/mpi/8.1.7/out/test.bat
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=1:00:00
+#PBS -q dev
+#PBS -A GFS-DEV
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module unload PrgEnv-cray PrgEnv-intel
+module load PrgEnv-gnu cray-pals craype cmake
+module load gcc/10.3.0 cray-mpich/8.1.7
+module load netcdf
+module load hdf5
+
+set -x
+export ESMF_MPIRUN=mpirun.unicos
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_CXXLINKOPTS="-fPIC -lnetcdff -lnetcdff"
+export ESMF_NETCDF=nc-config
+sed -i 's/aprun/mpiexec/' scripts/mpirun.unicos
+export ESMF_DIR=/lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_O_feature_large-messages/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_O_feature_large-messages/module-test.log
+cd /lfs/h1/emc/ptmp/Mark.Potts/gfortran_10.3.0_mpi_O_feature_large-messages/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
