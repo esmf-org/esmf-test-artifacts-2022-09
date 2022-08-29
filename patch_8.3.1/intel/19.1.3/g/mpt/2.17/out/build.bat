69c92f65936ed93bb3197b3164db2f114063fd83
Mark.Potts <Mark.Potts@noaa.gov>
2022-08-29 02:13:37 -0400

    action=collect dir=intel_19.1.3_mpt_g_patch_8.3.1 branch=patch_8.3.1 hash=v8.3.1b05 phase=test

---
+++ patch_8.3.1/intel/19.1.3/g/mpt/2.17/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_mpt_g_patch_8.3.1/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_mpt_g_patch_8.3.1/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+export MPICXX_CXX=icpc
+export MPICC_CC=icc
+module load comp/intel/19.1.3.304 mpi/sgi-mpt/2.17
+module load netcdf4/4.7.4
+module load hdf5/1.13.0
+
+set -x
+export ESMF_NETCDF_LIBS="-lhdf5_hl -lhdf5"
+export ESMF_DIR=/discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_mpt_g_patch_8.3.1/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_mpt_g_patch_8.3.1/module-build.log
+cd /discover/nobackup/projects/sbu/mpotts/esmf-test/intel_19.1.3_mpt_g_patch_8.3.1/esmf
+set -o pipefail
+make info 2>&1| tee ../info.log
+make -j 28 2>&1| tee ../build.log
